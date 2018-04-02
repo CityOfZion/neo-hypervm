d1764d230b090d3320a5a61de19155b54bb02f62
Shargon <shargon@gmail.com>
2018-04-02 14:01:55 +0200

    Fix name

--- NeoVM/SHA256.cpp
+++ NeoVM/SHA256.cpp
@@ -2,7 +2,7 @@
 
 #include <cstring>
 #include <fstream>
-#include "sha256.h"
+#include "SHA256.h"
 
 const unsigned int SHA256::sha256_k[64] = //UL = uint32
 { 0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
