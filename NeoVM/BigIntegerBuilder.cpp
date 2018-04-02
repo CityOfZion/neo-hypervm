bbc3a3446e0da687e8bf2c852129558425498141
Shargon <shargon@gmail.com>
2018-04-02 20:55:11 +0200

    INC, ADD, SUM and DEC

--- NeoVM/BigIntegerBuilder.cpp
+++ NeoVM/BigIntegerBuilder.cpp
@@ -1,35 +1,5 @@
 #include "BigIntegerBuilder.h"
 
-/*
-BigIntegerBuilder::BigIntegerBuilder(int32 sign, uint32 *bits, int32 bitSize)
-{
-	this->_rguLength = bitSize;
-	this->disposable = true;
-
-	if (bits == 0)
-	{
-		this->_rgu = 0;
-		this->_iuLast = 0;
-
-		// _uSmall = NumericsHelpers.Abs(bn._Sign);
-		uint32 mask = (uint32)(sign >> 31);
-		this->_uSmall = ((uint32)sign ^ mask) - mask;
-	}
-	else
-	{
-		this->_rgu = new uint32[bitSize];
-		for (int32 i = 0; i < bitSize; i++) this->_rgu[i] = bits[i];
-
-		this->_iuLast = this->_rguLength - 1;
-		this->_uSmall = this->_rgu[0];
-		while (this->_iuLast > 0 && this->_rgu[this->_iuLast] == 0)
-			--this->_iuLast;
-	}
-
-	// AssertValid(true);
-}
-*/
-
 BigIntegerBuilder::BigIntegerBuilder(int32 sign, uint32 * bits, int32 bitSize, int32 &outSign)
 {
 	this->_fWritable = false;
@@ -39,7 +9,7 @@ BigIntegerBuilder::BigIntegerBuilder(int32 sign, uint32 * bits, int32 bitSize, i
 	int32 n = sign;
 	int32 mask = n >> (kcbitUint - 1);
 	outSign = (outSign ^ mask) - mask;
-	
+
 	if (this->_rgu == NULL)
 	{
 		this->_iuLast = 0;
@@ -52,7 +22,7 @@ BigIntegerBuilder::BigIntegerBuilder(int32 sign, uint32 * bits, int32 bitSize, i
 		while (this->_iuLast > 0 && this->_rgu[this->_iuLast] == 0)
 			--this->_iuLast;
 	}
-	
+
 	// AssertValid(true);
 }
 
@@ -210,55 +180,370 @@ void BigIntegerBuilder::Mul(BigIntegerBuilder &regMul)
 	*/
 }
 
+// Apply a single borrow starting at iu. This does NOT trim the result.
+void BigIntegerBuilder::ApplyBorrow(int32 iuMin)
+{
+	// Contract.Requires(0 < iuMin);
+	// Contract.Assert(_fWritable && _iuLast > 0);
+	// Contract.Assert(iuMin <= _iuLast);
+
+	for (int32 iu = iuMin; iu <= this->_iuLast; iu++)
+	{
+		uint32 u = this->_rgu[iu]--;
+		if (u > 0)
+			return;
+	}
+	// Borrowed off the end!
+	// Contract.Assert(false, "Invalid call to ApplyBorrow");
+}
+// Makes sure the buffer is writable and can support cu uints.
+// Preserves the contents of the buffer up to min(_iuLast + 1, cu).
+// Changes the size if cu <= _iuLast and the buffer needs to be allocated.
+void BigIntegerBuilder::EnsureWritable(int32 cu, int32 cuExtra)
+{
+	// Contract.Requires(cu > 1 && cuExtra >= 0);
+	// AssertValid(false);
+
+	if (this->_fWritable && this->_rguLength >= cu)
+		return;
+
+	uint32 *rgu = new uint32[cu + cuExtra];
+
+	if (this->_iuLast > 0)
+	{
+		if (this->_iuLast >= cu)
+			this->_iuLast = cu - 1;
+
+		// Array.Copy(_rgu, rgu, _iuLast + 1);
+		for (int x = this->_iuLast; x >= 0; x--)
+			rgu[x] = this->_rgu[x];
+	}
+
+	this->_rguLength = cu + cuExtra;
+	this->_rgu = rgu;
+	this->_fWritable = true;
+
+	// AssertValid(false);
+}
+
+// Makes sure the buffer is writable and can support _iuLast + 1 uints.
+// Preserves the contents of the buffer.
+void BigIntegerBuilder::EnsureWritable(int32 cuExtra)
+{
+	// Contract.Requires(cuExtra >= 0);
+	// AssertValid(false);
+	// Contract.Assert(_iuLast > 0);
+
+	if (this->_fWritable)
+	{
+		// TODO: Check size??
+		return;
+	}
+
+	uint32 *rgu = new uint32[_iuLast + 1 + cuExtra];
+
+	for (int x = _iuLast + cuExtra; x >= 0; x--)
+		rgu[x] = this->_rgu[x];
+
+	this->_rgu = rgu;
+	this->_rguLength = _iuLast + 1 + cuExtra;
+	this->_fWritable = true;
+
+	// AssertValid(false);
+}
+
+// Sets the size to cu and makes sure the buffer is writable (if cu > 1),
+// but makes no guarantees about the contents of the buffer.
+void BigIntegerBuilder::SetSizeLazy(int32 cu)
+{
+	// Contract.Requires(cu > 0);
+	// AssertValid(false);
+
+	if (cu <= 1)
+	{
+		this->_iuLast = 0;
+		return;
+	}
+	if (!this->_fWritable || this->_rguLength < cu)
+	{
+		if (this->_fWritable && this->_rgu != NULL)
+			delete[](this->_rgu);
+
+		this->_rguLength = cu;
+		this->_rgu = new uint32[cu];
+		this->_fWritable = true;
+	}
+
+	this->_iuLast = cu - 1;
+	//AssertValid(false);
+}
+
+uint32 BigIntegerBuilder::AddCarry(uint32 &u1, uint32 u2, uint32 uCarry)
+{
+	uint64 uu = (uint64)u1 + u2 + uCarry;
+	u1 = (uint32)uu;
+	return (uint32)(uu >> kcbitUint);
+}
+
+uint32 BigIntegerBuilder::SubBorrow(uint32 &u1, uint32 u2, uint32 uBorrow)
+{
+	uint64 uu = (uint64)u1 - u2 - uBorrow;
+	u1 = (uint32)uu;
+	return (uint32)-(int)(uu >> kcbitUint);
+}
+
+// Apply a single carry starting at iu, extending the register
+// if needed.
+void BigIntegerBuilder::ApplyCarry(int32 iu)
+{
+	// Contract.Requires(0 <= iu);
+	// Contract.Assert(_fWritable && _iuLast > 0);
+	// Contract.Assert(iu <= _iuLast + 1);
+
+	for (; ; iu++)
+	{
+		if (iu > this->_iuLast)
+		{
+			if (this->_iuLast + 1 == this->_rguLength)
+			{
+				//Array.Resize(ref _rgu, _iuLast + 2);
+				uint32 * nrgu = new uint32[_iuLast + 2];
+				for (int x = _iuLast + 1; x >= 0; x--)
+					nrgu[x] = this->_rgu[x];
+
+				// TODO: Check this!
+				this->_rgu = nrgu;
+			}
+			this->_rgu[++this->_iuLast] = 1;
+			break;
+		}
+		if (++this->_rgu[iu] > 0)
+			break;
+	}
+}
+
+// Loads the value of reg into this register. If we need to allocate memory
+// to perform the load, allocate cuExtra elements.
+void BigIntegerBuilder::Load(BigIntegerBuilder &reg, int cuExtra)
+{
+	// Contract.Requires(cuExtra >= 0);
+	// AssertValid(false);
+	// reg.AssertValid(true);
+
+	if (reg._iuLast == 0)
+	{
+		this->_uSmall = reg._uSmall;
+		this->_iuLast = 0;
+	}
+	else
+	{
+		if (!this->_fWritable || this->_rguLength <= reg._iuLast)
+		{
+			this->_rgu = new uint32[reg._iuLast + 1 + cuExtra];
+			this->_rguLength = reg._iuLast + 1 + cuExtra;
+			this->_fWritable = true;
+		}
+
+		this->_iuLast = reg._iuLast;
+		//Array.Copy(reg._rgu, _rgu, _iuLast + 1);
+		for (int x = _iuLast; x >= 0; x--)
+			this->_rgu[x] = reg._rgu[x];
+	}
+
+	// AssertValid(true);
+}
+
+void BigIntegerBuilder::Add(uint32 u)
+{
+	// AssertValid(true);
+
+	if (this->_iuLast == 0)
+	{
+		if ((this->_uSmall += u) >= u)
+			return;
+
+		SetSizeLazy(2);
+		this->_rgu[0] = this->_uSmall;
+		this->_rgu[1] = 1;
+		return;
+	}
+
+	if (u == 0)
+		return;
+
+	uint32 uNew = this->_rgu[0] + u;
+	if (uNew < u)
+	{
+		// Have carry.
+		EnsureWritable(1);
+		ApplyCarry(1);
+	}
+	else if (!this->_fWritable)
+	{
+		EnsureWritable(0);
+	}
+
+	this->_rgu[0] = uNew;
+
+	// AssertValid(true);
+}
+
 void BigIntegerBuilder::Add(BigIntegerBuilder &reg)
 {
 	// AssertValid(true);
 	// reg.AssertValid(true);
 
-	/*
-	  if (reg._iuLast == 0)
-	  {
+	if (reg._iuLast == 0)
+	{
 		Add(reg._uSmall);
 		return;
-	  }
-	  if (_iuLast == 0)
-	  {
-		uint32 u = _uSmall;
+	}
+	if (this->_iuLast == 0)
+	{
+		uint32 u = this->_uSmall;
 		if (u == 0)
-		  this = new BigIntegerBuilder(ref reg);
+		{
+			//this = new BigIntegerBuilder(ref reg);
+			Load(reg, 0);
+		}
 		else
 		{
-		  Load(ref reg, 1);
-		  Add(u);
+			Load(reg, 1);
+			Add(u);
 		}
 		return;
-	  }
+	}
 
-	  EnsureWritable(Math.Max(_iuLast, reg._iuLast) + 1, 1);
+	EnsureWritable((this->_iuLast > reg._iuLast ? this->_iuLast : reg._iuLast) + 1, 1);
 
-	  int32 cuAdd = reg._iuLast + 1;
-	  if (_iuLast < reg._iuLast)
-	  {
-		cuAdd = _iuLast + 1;
-		Array.Copy(reg._rgu, _iuLast + 1, _rgu, _iuLast + 1, reg._iuLast - _iuLast);
-		Contract.Assert(_iuLast > 0);
-		_iuLast = reg._iuLast;
-	  }
+	int32 cuAdd = reg._iuLast + 1;
+	if (this->_iuLast < reg._iuLast)
+	{
+		cuAdd = this->_iuLast + 1;
 
-	  // Add, tracking carry.
-	  uint32 uCarry = 0;
-	  for (int32 iu = 0; iu < cuAdd; iu++)
-	  {
-		uCarry = AddCarry(ref _rgu[iu], reg._rgu[iu], uCarry);
-		Contract.Assert(uCarry <= 1);
-	  }
+		//Array.Copy(reg._rgu, _iuLast + 1, _rgu, _iuLast + 1, reg._iuLast - _iuLast);
+		for (int x = 0; x < reg._iuLast - this->_iuLast; x++)
+			this->_rgu[x + this->_iuLast + 1] = reg._rgu[x + this->_iuLast + 1];
 
-	  // Deal with extra carry.
-	  if (uCarry != 0)
+		// Contract.Assert(_iuLast > 0);
+		this->_iuLast = reg._iuLast;
+	}
+
+	// Add, tracking carry.
+	uint32 uCarry = 0;
+	for (int32 iu = 0; iu < cuAdd; iu++)
+	{
+		uCarry = AddCarry(this->_rgu[iu], reg._rgu[iu], uCarry);
+		// Contract.Assert(uCarry <= 1);
+	}
+
+	// Deal with extra carry.
+	if (uCarry != 0)
 		ApplyCarry(cuAdd);
-	  */
 
-	  // AssertValid(true);
+	// AssertValid(true);
+}
+
+void BigIntegerBuilder::Trim()
+{
+	// AssertValid(false);
+	if (this->_iuLast > 0 && this->_rgu[this->_iuLast] == 0)
+	{
+		this->_uSmall = this->_rgu[0];
+		while (--this->_iuLast > 0 && this->_rgu[this->_iuLast] == 0);
+	}
+	// AssertValid(true);
+}
+
+int32 BigIntegerBuilder::GetDiffLength(uint32 *rgu1, uint32 * rgu2, int32 cu)
+{
+	for (int32 iv = cu; --iv >= 0; )
+	{
+		if (rgu1[iv] != rgu2[iv])
+			return iv + 1;
+	}
+	return 0;
+}
+
+uint32 BigIntegerBuilder::SubRevBorrow(uint32 &u1, uint32 u2, uint32 uBorrow)
+{
+	uint64 uu = (uint64)u2 - u1 - uBorrow;
+	u1 = (uint32)uu;
+	return (uint32)-(int)(uu >> kcbitUint);
+}
+
+// Subtract this register from the given one and put the result in this one.
+// Asserts that reg is larger in the most significant uint.
+void BigIntegerBuilder::SubRev(BigIntegerBuilder * reg)
+{
+	// Contract.Assert(0 < _iuLast && _iuLast <= reg._iuLast);
+	// Contract.Assert(_iuLast < reg._iuLast || _rgu[_iuLast] < reg._rgu[_iuLast]);
+
+	EnsureWritable(reg->_iuLast + 1, 0);
+
+	int cuSub = this->_iuLast + 1;
+	if (this->_iuLast < reg->_iuLast)
+	{
+		// Array.Copy(reg->_rgu, _iuLast + 1, _rgu, _iuLast + 1, reg->_iuLast - _iuLast);
+
+		int index = _iuLast + 1;
+		for (int x = reg->_iuLast - _iuLast - 1; x >= 0; x--)
+			this->_rgu[x + index] = reg->_rgu[x + index];
+
+		// Contract.Assert(_iuLast > 0);
+		_iuLast = reg->_iuLast;
+	}
+
+	uint32 uBorrow = 0;
+	for (int iu = 0; iu < cuSub; iu++)
+	{
+		uBorrow = SubRevBorrow(this->_rgu[iu], reg->_rgu[iu], uBorrow);
+		// Contract.Assert(uBorrow <= 1);
+	}
+	if (uBorrow != 0)
+	{
+		// Contract.Assert(uBorrow == 1);
+		ApplyBorrow(cuSub);
+	}
+	Trim();
+}
+
+void BigIntegerBuilder::Sub(int32 &sign, uint32 u)
+{
+	// Contract.Requires(sign == +1 || sign == -1);
+	// AssertValid(true);
+
+	if (this->_iuLast == 0)
+	{
+		if (u <= this->_uSmall)
+		{
+			this->_uSmall -= u;
+		}
+		else
+		{
+			this->_uSmall = u - this->_uSmall;
+			sign = -sign;
+		}
+
+		// AssertValid(true);
+		return;
+	}
+
+	if (u == 0)
+		return;
+
+	EnsureWritable(0);
+
+	uint32 uTmp = this->_rgu[0];
+	this->_rgu[0] = uTmp - u;
+
+	if (uTmp < u)
+	{
+		ApplyBorrow(1);
+		Trim();
+	}
+
+	// AssertValid(true);
 }
 
 void BigIntegerBuilder::Sub(int32 &sign, BigIntegerBuilder &reg)
@@ -267,88 +552,94 @@ void BigIntegerBuilder::Sub(int32 &sign, BigIntegerBuilder &reg)
 	// AssertValid(true);
 	// reg.AssertValid(true);
 
-	/*
-	  if (reg._iuLast == 0)
-	  {
-		Sub(ref sign, reg._uSmall);
+	if (reg._iuLast == 0)
+	{
+		Sub(sign, reg._uSmall);
 		return;
-	  }
-	  if (_iuLast == 0)
-	  {
-		uint32 u = _uSmall;
+	}
+	if (this->_iuLast == 0)
+	{
+		uint32 u = this->_uSmall;
 		if (u == 0)
-		  this = new BigIntegerBuilder(ref reg);
+		{
+			// this = new BigIntegerBuilder(ref reg);
+			Load(reg, 0);
+		}
 		else
 		{
-		  Load(ref reg);
-		  Sub(ref sign, u);
+			Load(reg, 0);
+			Sub(sign, u);
 		}
 		sign = -sign;
 		return;
-	  }
+	}
 
-	  if (_iuLast < reg._iuLast)
-	  {
-		SubRev(ref reg);
+	if (this->_iuLast < reg._iuLast)
+	{
+		SubRev(&reg);
 		sign = -sign;
 		return;
-	  }
+	}
 
-	  int32 cuSub = reg._iuLast + 1;
-	  if (_iuLast == reg._iuLast)
-	  {
+	int32 cuSub = reg._iuLast + 1;
+	if (this->_iuLast == reg._iuLast)
+	{
 		// Determine which is larger.
-		_iuLast = BigInteger.GetDiffLength(_rgu, reg._rgu, _iuLast + 1) - 1;
-		if (_iuLast < 0)
+		this->_iuLast = GetDiffLength(this->_rgu, reg._rgu, this->_iuLast + 1) - 1;
+		if (this->_iuLast < 0)
 		{
-		  _iuLast = 0;
-		  _uSmall = 0;
-		  return;
+			this->_iuLast = 0;
+			this->_uSmall = 0;
+			return;
 		}
 
-		uint32 u1 = _rgu[_iuLast];
-		uint32 u2 = reg._rgu[_iuLast];
-		if (_iuLast == 0)
+		uint32 u1 = this->_rgu[this->_iuLast];
+		uint32 u2 = reg._rgu[this->_iuLast];
+		if (this->_iuLast == 0)
 		{
-		  if (u1 < u2) {
-			_uSmall = u2 - u1;
-			sign = -sign;
-		  }
-		  else
-			_uSmall = u1 - u2;
-		  AssertValid(true);
-		  return;
+			if (u1 < u2)
+			{
+				this->_uSmall = u2 - u1;
+				sign = -sign;
+			}
+			else
+			{
+				this->_uSmall = u1 - u2;
+			}
+
+			// AssertValid(true);
+			return;
 		}
 
 		if (u1 < u2)
 		{
-		  Contract.Assert(_iuLast > 0);
-		  reg._iuLast = _iuLast;
-		  SubRev(ref reg);
-		  reg._iuLast = cuSub - 1;
-		  Contract.Assert(reg._iuLast > 0);
-		  sign = -sign;
-		  return;
+			// Contract.Assert(_iuLast > 0);
+			reg._iuLast = this->_iuLast;
+			SubRev(&reg);
+			reg._iuLast = cuSub - 1;
+			// Contract.Assert(reg._iuLast > 0);
+			sign = -sign;
+			return;
 		}
-		cuSub = _iuLast + 1;
-	  }
+		cuSub = this->_iuLast + 1;
+	}
 
-	  EnsureWritable();
+	EnsureWritable(0);
 
-	  // Subtract, tracking borrow.
-	  uint32 uBorrow = 0;
-	  for (int32 iu = 0; iu < cuSub; iu++)
-	  {
-		uBorrow = SubBorrow(ref _rgu[iu], reg._rgu[iu], uBorrow);
-		Contract.Assert(uBorrow <= 1);
-	  }
-	  if (uBorrow != 0)
-	  {
-		Contract.Assert(uBorrow == 1 && cuSub <= _iuLast);
+	// Subtract, tracking borrow.
+	uint32 uBorrow = 0;
+	for (int32 iu = 0; iu < cuSub; iu++)
+	{
+		uBorrow = SubBorrow(this->_rgu[iu], reg._rgu[iu], uBorrow);
+		// Contract.Assert(uBorrow <= 1);
+	}
+	if (uBorrow != 0)
+	{
+		// Contract.Assert(uBorrow == 1 && cuSub <= _iuLast);
 		ApplyBorrow(cuSub);
-	  }
-	  Trim();
-	*/
+	}
+
+	Trim();
 }
 
 BigIntegerBuilder::~BigIntegerBuilder()
--- NeoVM/BigIntegerBuilder.h
+++ NeoVM/BigIntegerBuilder.h
@@ -22,6 +22,23 @@ private:
 
 	int32 _rguLength;
 
+	static uint32 AddCarry(uint32 &u1, uint32 u2, uint32 uCarry);
+	static int32 GetDiffLength(uint32 *rgu1, uint32 * rgu2, int32 cu);
+	static uint32 SubBorrow(uint32 &u1, uint32 u2, uint32 uBorrow);
+	uint32 SubRevBorrow(uint32 &u1, uint32 u2, uint32 uBorrow);
+	
+	void ApplyBorrow(int32 iuMin);
+	void Add(uint32 u);
+	void Sub(int32 &sign, uint32 u);
+	void SubRev(BigIntegerBuilder * reg);
+
+	void Trim();
+	void SetSizeLazy(int32 cu);
+	void EnsureWritable(int32 cuExtra);
+	void EnsureWritable(int32 cu, int32 cuExtra);
+	void ApplyCarry(int32 iu);
+	void Load(BigIntegerBuilder &reg, int cuExtra);
+
 public:
 
 	// Methods
@@ -35,7 +52,6 @@ public:
 
 	// Constructor
 
-	//BigIntegerBuilder(int32 sign, uint32 * bits, int32 bitSize);
 	BigIntegerBuilder(int32 sign, uint32 * bits, int32 bitSize, int32 &outSign);
 
 	// Destructor
