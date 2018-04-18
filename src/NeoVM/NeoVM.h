97065774dbb4a3b5fd569635ebbe15a53191bf35
Shargon <shargon@gmail.com>
2018-04-19 00:12:10 +0200

    Fixed Makefile

--- src/NeoVM/NeoVM.h
+++ src/NeoVM/NeoVM.h
@@ -7,7 +7,7 @@
 
 #if defined(_WINDOWS)
 // Windows
-#define DllExport __declspec( dllexport ) 
+#define DllExport __declspec( dllexport )
 #elif defined (__GNUC__)
 // GCC
 #define DllExport __attribute__((visibility("default"))) 
@@ -17,65 +17,65 @@ extern "C"
 {
 	// Get version
 
-	DllExport void GetVersion(int32 &major, int32 &minor, int32 &build, int32 &revision);
+	DllExport void __stdcall GetVersion(int32 &major, int32 &minor, int32 &build, int32 &revision);
 
 	// ExecutionContext
 
-	DllExport int32 ExecutionContext_GetScriptHash(ExecutionContext* context, byte *output, int32 index);
-	DllExport EVMOpCode ExecutionContext_GetNextInstruction(ExecutionContext* context);
-	DllExport int32 ExecutionContext_GetInstructionPointer(ExecutionContext* context);
-	DllExport void ExecutionContext_Claim(ExecutionContext* context);
-	DllExport void ExecutionContext_Free(ExecutionContext* &context);
+	DllExport int32 __stdcall ExecutionContext_GetScriptHash(ExecutionContext* context, byte *output, int32 index);
+	DllExport EVMOpCode __stdcall ExecutionContext_GetNextInstruction(ExecutionContext* context);
+	DllExport int32 __stdcall ExecutionContext_GetInstructionPointer(ExecutionContext* context);
+	DllExport void __stdcall ExecutionContext_Claim(ExecutionContext* context);
+	DllExport void __stdcall ExecutionContext_Free(ExecutionContext* &context);
 
 	// ExecutionEngine
 
-	DllExport ExecutionEngine* ExecutionEngine_Create
+	DllExport ExecutionEngine* __stdcall ExecutionEngine_Create
 	(
 		InvokeInteropCallback interopCallback, LoadScriptCallback getScriptCallback, GetMessageCallback getMessageCallback,
 		ExecutionContextStack* &invStack, StackItems* &evStack, StackItems* &altStack
 	);
-	DllExport void ExecutionEngine_Free(ExecutionEngine* &engine);
-	DllExport void ExecutionEngine_Clean(ExecutionEngine* engine, uint32 iteration);
-	DllExport void ExecutionEngine_LoadScript(ExecutionEngine* engine, byte * script, int32 scriptLength);
-	DllExport void ExecutionEngine_LoadPushOnlyScript(ExecutionEngine* engine, byte * script, int32 scriptLength);
-	DllExport byte ExecutionEngine_Execute(ExecutionEngine* engine);
-	DllExport void ExecutionEngine_StepInto(ExecutionEngine* engine);
-	DllExport void ExecutionEngine_StepOver(ExecutionEngine* engine);
-	DllExport void ExecutionEngine_StepOut(ExecutionEngine* engine);
-	DllExport int32 ExecutionEngine_GetState(ExecutionEngine* engine);
-	DllExport void ExecutionEngine_AddLog(ExecutionEngine* engine, OnStepIntoCallback callback);
+	DllExport void __stdcall ExecutionEngine_Free(ExecutionEngine* &engine);
+	DllExport void __stdcall ExecutionEngine_Clean(ExecutionEngine* engine, uint32 iteration);
+	DllExport void __stdcall ExecutionEngine_LoadScript(ExecutionEngine* engine, byte * script, int32 scriptLength);
+	DllExport void __stdcall ExecutionEngine_LoadPushOnlyScript(ExecutionEngine* engine, byte * script, int32 scriptLength);
+	DllExport byte __stdcall ExecutionEngine_Execute(ExecutionEngine* engine);
+	DllExport void __stdcall ExecutionEngine_StepInto(ExecutionEngine* engine);
+	DllExport void __stdcall ExecutionEngine_StepOver(ExecutionEngine* engine);
+	DllExport void __stdcall ExecutionEngine_StepOut(ExecutionEngine* engine);
+	DllExport int32 __stdcall ExecutionEngine_GetState(ExecutionEngine* engine);
+	DllExport void __stdcall ExecutionEngine_AddLog(ExecutionEngine* engine, OnStepIntoCallback callback);
 
 	// StackItems
 
-	DllExport int32 StackItems_Count(StackItems* stack);
-	DllExport IStackItem* StackItems_Pop(StackItems* stack);
-	DllExport IStackItem* StackItems_Peek(StackItems* stack, int32 index);
-	DllExport void StackItems_Push(StackItems* stack, IStackItem *item);
-	DllExport int32 StackItems_Drop(StackItems* stack, int32 count);
-	DllExport void StackItems_AddLog(StackItems* stack, OnStackChangeCallback callback);
+	DllExport int32 __stdcall StackItems_Count(StackItems* stack);
+	DllExport IStackItem* __stdcall StackItems_Pop(StackItems* stack);
+	DllExport IStackItem* __stdcall StackItems_Peek(StackItems* stack, int32 index);
+	DllExport void __stdcall StackItems_Push(StackItems* stack, IStackItem *item);
+	DllExport int32 __stdcall StackItems_Drop(StackItems* stack, int32 count);
+	DllExport void __stdcall StackItems_AddLog(StackItems* stack, OnStackChangeCallback callback);
 
 	// ExecutionContextStack
 
-	DllExport int32 ExecutionContextStack_Count(ExecutionContextStack* stack);
-	DllExport int32 ExecutionContextStack_Drop(ExecutionContextStack* stack, int32 count);
-	DllExport ExecutionContext* ExecutionContextStack_Peek(ExecutionContextStack* stack, int32 index);
-	DllExport void ExecutionContextStack_AddLog(ExecutionContextStack* stack, OnStackChangeCallback callback);
+	DllExport int32 __stdcall ExecutionContextStack_Count(ExecutionContextStack* stack);
+	DllExport int32 __stdcall ExecutionContextStack_Drop(ExecutionContextStack* stack, int32 count);
+	DllExport ExecutionContext* __stdcall ExecutionContextStack_Peek(ExecutionContextStack* stack, int32 index);
+	DllExport void __stdcall ExecutionContextStack_AddLog(ExecutionContextStack* stack, OnStackChangeCallback callback);
 
 	// StackItem
 
-	DllExport IStackItem* StackItem_Create(EStackItemType type, byte *data, int32 size);
-	DllExport EStackItemType StackItem_SerializeInfo(IStackItem* item, int32 &size);
-	DllExport int32 StackItem_Serialize(IStackItem* item, byte * output, int32 length);
-	DllExport void StackItem_Free(IStackItem* &item);
+	DllExport IStackItem* __stdcall StackItem_Create(EStackItemType type, byte *data, int32 size);
+	DllExport EStackItemType __stdcall StackItem_SerializeInfo(IStackItem* item, int32 &size);
+	DllExport int32 __stdcall StackItem_Serialize(IStackItem* item, byte * output, int32 length);
+	DllExport void __stdcall StackItem_Free(IStackItem* &item);
 
 	// ArrayStackItem
 
-	DllExport int32 ArrayStackItem_Count(ArrayStackItem* array);
-	DllExport void ArrayStackItem_Clear(ArrayStackItem* array);
-	DllExport IStackItem* ArrayStackItem_Get(ArrayStackItem* array, int32 index);
-	DllExport void ArrayStackItem_Add(ArrayStackItem* array, IStackItem* item);
-	DllExport void ArrayStackItem_Set(ArrayStackItem* array, IStackItem* item, int32 index);
-	DllExport int32 ArrayStackItem_IndexOf(ArrayStackItem* array, IStackItem* item);
-	DllExport void ArrayStackItem_Insert(ArrayStackItem* array, IStackItem* item, int32 index);
-	DllExport void ArrayStackItem_RemoveAt(ArrayStackItem* array, int32 index);
+	DllExport int32 __stdcall ArrayStackItem_Count(ArrayStackItem* array);
+	DllExport void __stdcall ArrayStackItem_Clear(ArrayStackItem* array);
+	DllExport IStackItem* __stdcall ArrayStackItem_Get(ArrayStackItem* array, int32 index);
+	DllExport void __stdcall ArrayStackItem_Add(ArrayStackItem* array, IStackItem* item);
+	DllExport void __stdcall ArrayStackItem_Set(ArrayStackItem* array, IStackItem* item, int32 index);
+	DllExport int32 __stdcall ArrayStackItem_IndexOf(ArrayStackItem* array, IStackItem* item);
+	DllExport void __stdcall ArrayStackItem_Insert(ArrayStackItem* array, IStackItem* item, int32 index);
+	DllExport void __stdcall ArrayStackItem_RemoveAt(ArrayStackItem* array, int32 index);
 }
