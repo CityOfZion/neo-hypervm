0e313edcb62de9f38bfd7c82adb600ac79b3f1f4
Belane <srbelane@gmail.com>
2018-09-14 00:38:34 +0200

    osx travis integration

--- scripts/testAndCompile.sh
+++ scripts/testAndCompile.sh
@@ -7,7 +7,11 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 
 cd $SCRIPTPATH/../tests/
 
-export NEO_VM_PATH=$SCRIPTPATH/../src/bin/Neo.HyperVM.so
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    export NEO_VM_PATH=$SCRIPTPATH/../src/bin/Neo.HyperVM.dylib
+else
+    export NEO_VM_PATH=$SCRIPTPATH/../src/bin/Neo.HyperVM.so
+fi
 
 echo "************************"
 echo "**      UNIT TEST     **"
