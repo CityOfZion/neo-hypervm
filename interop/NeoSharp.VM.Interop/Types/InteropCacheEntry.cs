6b0ddd3764007ca6cf1c3c734bbe7eb87325be06
Shargon <shargon@gmail.com>
2018-11-29 19:41:11 +0100

    Revert "Clean code"
    This reverts commit 79ead5a317029f3836fb72626d152a3f788b8b97.

--- interop/NeoSharp.VM.Interop/Types/InteropCacheEntry.cs
+++ interop/NeoSharp.VM.Interop/Types/InteropCacheEntry.cs
@@ -6,8 +6,6 @@ namespace NeoSharp.VM.Interop.Types
     {
         public object Value;
 
-        public int Index;
-
         public Func<ExecutionEngine, IntPtr, int, object, StackItemBase> Instanciator;
     }
 }
